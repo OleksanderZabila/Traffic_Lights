a9e4ca755daa1d0847ba55383f4cc92e6ac8bdb3
Zabila <126409203+OleksanderZabila@users.noreply.github.com>
2023-02-27 22:09:14 +0200

    Update import time.py

--- import time.py
+++ import time.py
@@ -2,57 +2,49 @@ import time
 
 class TrafficLightControl:
     def __init__(self):
-        self.traffic_light_control = TrafficLightControl
         self.traffic_light = TrafficLight()
         self.light_red = Red()
         self.light_yellow = Yellow()
         self.light_green = Green()
 
     def run(self):
-        while True:
-            self.traffic_light.change_light(self.traffic_light.red)
-            time.sleep(5)
-            self.traffic_light.change_light(self.traffic_light.yellow)
-            time.sleep(2)
-            self.traffic_light.change_light(self.traffic_light.green)
-            time.sleep(5)
+        self.change_light()
 
     def stop(self):
-        self.traffic_light.changelight("stop")
-
-class TrafficLight:
-    def __init__(self):
-        self.light = None
-        self.red = Red()
-        self.yellow = Yellow()
-        self.green = Green()
-
-    def change_light(self, light):
-        for other_light in [self.red, self.yellow, self.green]:
-            if other_light != light:
-                other_light.GetColor()
-        
-
-class Light:
-    def GetColor(self):
         pass
 
+    def change_light(self):
+     print(self.light_red.get_color())
+     time.sleep(5)
+     print(self.light_yellow.get_color())
+     time.sleep(2)
+     print(self.light_green.get_color())
+     time.sleep(5)
+
+
+class TrafficLight:
+    def change_light(self, light):
+        pass
+
+
+class Light:
+    def get_color(self):
+        pass
+
+
 class Red(Light):
-    def GetColor(self):
-        print("Red")
+    def get_color(self):
         return "Red"
 
 
 class Green(Light):
-    def GetColor(self):
-        print("Yellow")
+    def get_color(self):
         return "Green"
-     
+
 
 class Yellow(Light):
-    def GetColor(self):
-        print("Green")
+    def get_color(self):
         return "Yellow"
- 
+
 traffic_light_control = TrafficLightControl()
 traffic_light_control.run()
